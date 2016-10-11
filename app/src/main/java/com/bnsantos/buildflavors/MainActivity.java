cf1f822ad534f08a22240c1276447427f25996f6
Bruno Santos <brunonascimentosantos@gmail.com>
2016-10-11 11:42:30 -0300

    simple flavors

--- app/src/main/java/com/bnsantos/buildflavors/MainActivity.java
+++ app/src/main/java/com/bnsantos/buildflavors/MainActivity.java
@@ -1,13 +1,19 @@
 package com.bnsantos.buildflavors;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bnsantos.buildflavors.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding mBinding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    mBinding.text.setText(BuildConfig.TEST);
   }
 }
